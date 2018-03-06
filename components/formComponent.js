b8ea9cdcb933a17e603503b3029ec5e74b4b280a
George Morris <gmorris1242@gmail.com>
2018-03-06 09:59:34 -0500

    update buttons

--- components/formComponent.js
+++ components/formComponent.js
@@ -2,9 +2,9 @@
   var formComponent = {
     template:`    <div class="mainForm">
           <h1>Title</h1>
-          <button type="button"><a href="#!/accident">Accident</a></button>
+          <button type="button"><a href="#!/accident">Car Accident</a></button>
+          <button type="button"><a href="#!/assault">Hit and Run</a></button>
           <button type="button"><a href="#!/theft">Theft</a></button>
-          <button type="button"><a href="#!/assault">Assault</a></button>
           <button type="button"><a href="#!/witness">Witness</a></button>
         </div>
 
