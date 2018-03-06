344fda2071f56f4913f7d201974efb3fd882efdd
kristibyrnes <kristibyrnes@icloud.com>
2018-03-05 21:57:12 -0500

    added input

--- components/assaultComponent.js
+++ components/assaultComponent.js
@@ -16,7 +16,7 @@
     <label>Who was involed?<input type="text" ng-model="$ctrl.action.who"></label>
     <label>What time did the action take place?<input type="text" ng-model="$ctrl.action.when"></label>
     <label>Where did the action occur?<input type="text" ng-model="$ctrl.action.where"></label>
-    <label>What happend?</label>
+    <label>What happened?  Include any events leading to or immediately following the incidient.</label>
     <textarea name="attacker" rows="8" cols="80" ng-model="$ctrl.action.what"></textarea>
     <button ng-click="$ctrl.goBack()" type="button">BACK</button>
     <button type="submit">NEXT</button>
@@ -34,23 +34,29 @@
   </div>
 
   <div class="tab assaultForm4" ng-switch-when="4">
-    <p>Remember to check the area for witnesses</p>
-    <p>Do not persue the conflict</p>
-    <p>Take photos</p>
-    <button ng-click="$ctrl.goBack()" type="button">BACK</button>
-    <button ng-click="$ctrl.getForm(5)" >NEXT</button>
+    <p>Did you see any witnesses? Please describe them and their response to the incident</p>
+
+    <form ng-submit="$ctrl.possibleWitnesses($ctrl.action)">
+      <textarea name="attacker" rows="8" cols="80" ng-model="$ctrl.action.response"></textarea>
+      <ul>Remember to:
+        <li>Not persue the attacker</li>
+        <li>Take photos</li>
+      </ul>
+      <button ng-click="$ctrl.goBack()" type="button">BACK</button>
+      <button type="submit">NEXT</button>
+    </form>
   </div>
 
   <div class="tab" ng-switch-when="5">
-    <p>Timeline of Events</p>
+    <p>Timeline of Event</p>
     <div>
-      <p>Description of event</p>
       <p>{{$ctrl.assaultDb.type}}</p>
       <p>Who was involved? {{$ctrl.assaultDb.who}}</p>
       <p>What time did the action take place? {{$ctrl.assaultDb.when}}</p>
       <p>Where did the action occur? {{$ctrl.assaultDb.where}}</p>
       <p>What happened? {{$ctrl.assaultDb.what}}</p>
       <p>Description of attacker: {{$ctrl.assaultDb.description}}</p>
+      <p>Any witnesses? {{$ctrl.assaultDb.response}}</p>
 
       <button ng-click="$ctrl.goBack()" type="button">BACK</button>
     </div>
@@ -82,6 +88,11 @@
       $ctrl.attackerDescription = function(action) {
         $ctrl.assaultDb.action = action.description;
         $ctrl.getForm(4);
+      }
+
+      $ctrl.possibleWitnesses = function(action) {
+        $ctrl.assaultDb.action = action.response;
+        $ctrl.getForm(5);
       }
     }
   }
