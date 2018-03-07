23a470ca63e3421af8601dbd0b2761745d1327ae
rbroda <brodaryan@gmail.com>
2018-03-07 11:18:32 -0500

    finished x button

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require("body-parser");
 var routes = require("./routes");
 
 var app = express();
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 app.use("/", routes);
 
