0ac09f7373d989236cabd6cdb3207e54762853a1
George Morris <gmorris1242@gmail.com>
2018-03-07 12:57:22 -0500

    adjust form css

--- public/components/formComponent.js
+++ public/components/formComponent.js
@@ -2,13 +2,13 @@
   var formComponent = {
     template:`
         <div class="mainForm tab">
-          <h1>Accidents Happen</h1>
-
-            <button class="mainButton" type="button"><a href="#!/accident">Car Accident</a></button>
-            <button class="mainButton" type="button"><a href="#!/hitandrun">Hit and Run</a></button>
-            <button class="mainButton" type="button"><a href="#!/theft">Theft</a></button>
-            <button class="mainButton" type="button"><a href="#!/witness">Witness</a></button>
-        
+        <h1>Accidents Happen</h1>
+        <div class = "formButtons">
+          <button class="mainButton" type="button"><a href="#!/accident">Car Accident</a></button>
+          <button class="mainButton" type="button"><a href="#!/hitandrun">Hit and Run</a></button>
+          <button class="mainButton" type="button"><a href="#!/theft">Theft</a></button>
+          <button class="mainButton" type="button"><a href="#!/witness">Witness</a></button>
+        </div>
         </div>
 
     `,
