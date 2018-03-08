74ebdd593cf7a8715284caaab034f162fc2ef6ea
kristibyrnes <kristibyrnes@icloud.com>
2018-03-08 09:19:40 -0500

    form styles

--- public/components/formComponent.js
+++ public/components/formComponent.js
@@ -1,15 +1,16 @@
 (function () {
   var formComponent = {
     template: `
-        <div class="mainForm tab">
+        <div id="mainForm" class="tab">
         <a class="xbut" href="#!/home"><i class="material-icons x">clear</i></a>
-        <h1>Accidents Happen</h1>
+        <h1 id="formTitle">Accidents Happen</h1>
         <div class = "formButtons">
-        <a class="mainButton" href="#!/accident"><button type="button">Car Accident</button></a>
-        <a class="mainButton" href="#!/hitandrun"><button type="button">Hit and Run</button></a>
-        <a class="mainButton" href="#!/theft"><button type="button">Theft</button></a>
-        <a class="mainButton" href="#!/witness"><button type="button">Witness</button></a>
+          <a class="mainButton" href="#!/accident"><button type="button">Car Accident</button></a>
+          <a class="mainButton" href="#!/hitandrun"><button type="button">Hit and Run</button></a>
+          <a class="mainButton" href="#!/theft"><button type="button">Theft</button></a>
+          <a class="mainButton" href="#!/witness"><button type="button">Witness</button></a>
         </div>
+        <a class="homeButton" href="#!/home"><button type="button">Home</button></a>
         </div>
 
     `,
