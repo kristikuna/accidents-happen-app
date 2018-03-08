2201bc2326b6513e9e5b7d11b9a5961dda7771db
George Morris <gmorris1242@gmail.com>
2018-03-08 12:07:51 -0500

    history page

--- public/components/previousComponent.js
+++ public/components/previousComponent.js
@@ -18,17 +18,48 @@
             <th>Type of incident:</th>
             <td>{{ incident.type_of | uppercase}}</td>
           </tr>
-
             <tr>
               <th>Events that took place:</th>
               <td>{{ incident.events | uppercase}}</td>
             </tr>
             <tr>
-              <th>Description of person:</th>
-              <td>{{ incident.driver | uppercase}}</td>
+              <th>Stolen or damaged property:</th>
+              <td>{{ incident.property ||"n/a" | uppercase}}</td>
+            </tr>
+            <tr>
+              <th>Description or name:</th>
+              <td>{{ incident.driver || incident.name_of ||"n/a" | uppercase}}</td>
+            </tr>
+            <tr>
+              <th>Contact info:</th>
+              <td>{{ incident.phone ||"n/a" | uppercase}}</td>
+            </tr>
+            <tr>
+              <th>Make:</th>
+              <td>{{ incident.make ||"n/a" | uppercase}}</td>
+            </tr>
+            <tr>
+              <th>Model:</th>
+              <td>{{ incident.model ||"n/a" | uppercase}}</td>
+            </tr>
+            <tr>
+              <th>Color:</th>
+              <td>{{ incident.color ||"n/a" | uppercase}}</td>
+            </tr>
+            <tr>
+              <th>Insurance info:</th>
+              <td>{{ incident.insurance_info ||"n/a" | uppercase}}</td>
+            </tr>
+            <tr>
+              <th>License Plate:</th>
+              <td>{{ incident.plate ||"n/a" | uppercase}}</td>
+            </tr>
+            <tr>
+              <th>Direction:</th>
+              <td>{{ incident.direction ||"n/a" | uppercase}}</td>
             </tr>
           </table>
-          <button ng-click="$ctrl.delete(incident.id)" style="margin-left: 20px;border-radius: 5px; padding: 2px">delete</button>
+          <button class="profileDelete" ng-click="$ctrl.delete(incident.id)">delete</button>
           </div>
         </div>
         `,
