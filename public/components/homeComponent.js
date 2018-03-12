5cb77daedfa857c0b63778c337f46a1f537a1adc
George Morris <gmorris1242@gmail.com>
2018-03-12 09:46:16 -0400

    link video page to homepage

--- public/components/homeComponent.js
+++ public/components/homeComponent.js
@@ -13,7 +13,7 @@
     <div class="homePageButtons" ng-style="$ctrl.display">
     <a id="previous" class="imgButton" href="#!/prev"><span class="imageBackgroundText" >History</span></a>
     <a id="help" class="imgButton" href="#!/form"><span class="imageBackgroundText" >Get Help</span></a>
-    <a id="article" class="imgButton" href="#!/"><span class="imageBackgroundText" >Articles</span></a>
+    <a id="article" class="imgButton" href="#!/videos"><span class="imageBackgroundText" >Articles</span></a>
     </div>
     `,
     controller: function(){
